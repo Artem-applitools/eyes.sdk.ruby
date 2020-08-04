7b1e5d835e68516fd5daf204de6316eac9e1498d
artem <tranduil15@gmail.com>
2020-08-04 22:17:54 +0300

    add tutorials checks from with docker

---
+++ tutorials/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cp -a ../pkg/gems/. ./gems
+docker build -t chrome_ruby .
+docker build -t ruby_selenium_basic -f selenium_basic/Dockerfile .
+docker build -t ruby_selenium_ufg -f selenium_ufg/Dockerfile .
+rm -r gems
---
+++ tutorials/checkTutorials.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker run -e APPLITOOLS_API_KEY ruby_selenium_basic
+docker run -e APPLITOOLS_API_KEY ruby_selenium_ufg
