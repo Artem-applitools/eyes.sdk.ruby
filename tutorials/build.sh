2eb5b32c8aa06c5a30fd2b818eb6815a4e0bcd23
artem <tranduil15@gmail.com>
2020-08-04 23:38:24 +0300

    move ruby docker image to dockerhub

--- tutorials/build.sh
+++ tutorials/build.sh
@@ -2,7 +2,6 @@
 
 ls
 cp -a ../pkg/gems/. ./gems
-docker build -t chrome_ruby .
 docker build -t ruby_selenium_basic -f selenium_basic/Dockerfile .
 docker build -t ruby_selenium_ufg -f selenium_ufg/Dockerfile .
 rm -r gems
--- tutorials/buildTravis.sh
+++ tutorials/buildTravis.sh
@@ -3,7 +3,6 @@
 echo $(pwd)
 ls
 cp -a ./pkg/gems/. ./tutorials/gems
-docker build -t chrome_ruby -f tutorials/Dockerfile .
 docker build -t ruby_selenium_basic -f tutorials/selenium_basic/Dockerfile .
 docker build -t ruby_selenium_ufg -f tutorials/selenium_ufg/Dockerfile .
 rm -r ./tutorials/gems
