a6bd86179f055efd98753dad72e24fa5bd9c6654
artem <tranduil15@gmail.com>
2020-08-05 15:30:08 +0300

    separate dockerfiles for travis and locally

--- tutorials/build.sh
+++ tutorials/build.sh
@@ -4,4 +4,6 @@ ls
 cp -a ../pkg/gems/. ./gems
 docker build -t ruby_selenium_basic -f selenium_basic/Dockerfile .
 docker build -t ruby_selenium_ufg -f selenium_ufg/Dockerfile .
+docker build -t ruby_capybara -f capybara/Dockerfile .
+docker build -t ruby_watir -f watir/Dockerfile .
 rm -r gems
--- tutorials/checkTutorials.sh
+++ tutorials/checkTutorials.sh
@@ -2,3 +2,5 @@
 
 docker run -e APPLITOOLS_API_KEY ruby_selenium_basic
 docker run -e APPLITOOLS_API_KEY ruby_selenium_ufg
+docker run -e APPLITOOLS_API_KEY ruby_capybara
+docker run -e APPLITOOLS_API_KEY ruby_watir
